7b9be51d5d8713edece7b08422ec011fe5f56a70
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-06-18 12:30:58 -0700

    normalizing matrix complete

--- absorbingMarkovChain.py
+++ absorbingMarkovChain.py
@@ -1,1 +1,36 @@
 # Stable matrix for an Absorbing Matrix
+
+def shiftRow(sub, steps):
+  sub = list(reversed( sub ))
+  sub[:steps] = sub[:steps][::-1]
+  sub[steps:] = sub[steps:][::-1]
+
+  return sub
+
+def answer(a):
+  size = len(a)
+
+  newMatrix = []
+  zeroArrays = []
+  nonZeros = []
+
+  for i in range( size ):
+
+    allZeros = True
+    for k in a[i]:
+      if k != 0:
+        allZeros = False
+
+    if allZeros:
+      zeroArrays.append( i )
+    else:
+      nonZeros.append( i )
+  # insert terminal rows in front
+  for z in range(len(zeroArrays)):
+    newMatrix.append( numpy.zeros( size ))
+    newMatrix[z][z] = 1
+  # insert rest of the rows
+  for r in range( len( nonZeros ) ):
+    z += 1
+    shift = z - nonZeros[r]
+    newMatrix.append( shiftRow( a[ nonZeros[r]], shift ))
