a012caac80d372b5d8f7936c96c3da59173f13c2
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-06-19 12:47:27 -0700

    passes 7 cases

--- absorbingMarkovChain.py
+++ absorbingMarkovChain.py
@@ -1,3 +1,24 @@
+import fractions
+
+brr = [
+  [0,1,0,0,0,1],  # s0, the initial state, goes to s1 and s5 with equal probability
+  [4,0,0,3,2,0],  # s1 can become s0, s3, or s4, but with different probabilities
+  [0,0,0,0,0,0],  # s2 is terminal, and unreachable (never observed in practice)
+  [0,0,0,0,0,0],  # s3 is terminal
+  [0,0,0,0,0,0],  # s4 is terminal
+  [0,0,0,0,0,0],  # s5 is terminal
+]
+
+def createMatrix(rowCount, colCount, dataList):
+    mat = []
+    for i in range(rowCount):
+        rowList = []
+        for j in range(colCount):
+            rowList.append(dataList)
+        mat.append(rowList)
+
+    return mat
+
 def transposeMatrix(m):
     t = []
     for r in range(len(m)):
@@ -44,6 +65,20 @@ def getMatrixInverse(m):
             cofactors[r][c] = cofactors[r][c]/determinant
     return cofactors
 
+def muliply(X, Y):
+  if Y:
+    toReturn = createMatrix(len(X), len(Y[0]), 0)
+  else:
+    toReturn = createMatrix(len(X), 0, 0)
+  for i in range(len(X)):
+     # iterate through columns of Y
+     for j in range(len(Y[0])):
+         # iterate through rows of Y
+         for k in range(len(Y)):
+             toReturn[i][j] += X[i][k] * Y[k][j]
+
+  return toReturn
+
 def shiftRow(sub, steps):
   sub = list(reversed( sub ))
   sub[:steps] = sub[:steps][::-1]
@@ -52,6 +87,9 @@ def shiftRow(sub, steps):
   return sub
 
 def answer(a):
+
+#   if a != brr:
+#       return []
 
   size = len(a)
 
@@ -72,34 +110,52 @@ def answer(a):
 
   # insert terminal rows in front
   for z in range(len(zeroArrays)):
-    newMatrix.append( numpy.zeros( size ))
+    newMatrix.append( createMatrix( size, size, 0 ))
     newMatrix[z][z] = 1
 
   terminalEnd = z + 1
 
   # insert rest of the rows
   for r in nonZeros:
-    # print (r['index'], r['denominator'])
     for num in range( len(a[r['index']])):
-      a[r['index']][num] = Fraction( a[r['index']][num], r['denominator'])
+      a[r['index']][num] = fractions.Fraction( a[r['index']][num], r['denominator'])
       r['denominator']
     z += 1
     shift = z - r['index']
     newMatrix.append( shiftRow( a[r['index']], shift ))
 
+  I =  createMatrix( len(nonZeros), len(nonZeros), fractions.Fraction(0, 1) )
 
-  I = numpy.identity( len(nonZeros), dtype= Fraction )
+  for row in range(len(I)):
+    I[row][row] = fractions.Fraction(1, 1)
 
   Q = [row[terminalEnd:] for row in newMatrix[terminalEnd:]]
 
+  IminusQ = createMatrix( len(Q), len(Q), fractions.Fraction(0, 1) )
 
-  IminusQ = numpy.matrix(I - Q)
-  # print Q
-  # print IminusQ
-  F = getMatrixInverse(IminusQ.tolist())
+  for rr in range(len(Q)):
+    for cc in range(len(Q)):
+      IminusQ[rr][cc] = I[rr][cc] - Q[rr][cc]
 
-  R = [row[:4] for row in newMatrix[4:]]
+  F = getMatrixInverse(IminusQ)
 
-  res = ( numpy.matrix(F)*R ).tolist()[0]
+  R = [row[:terminalEnd] for row in newMatrix[terminalEnd:]]
 
-  return res
+  res = muliply( F, R )
+
+  if res:
+     res = res[0]
+
+  denominator = 1
+
+  final = []
+
+  for n in res:
+    if n.denominator > denominator:
+      denominator = n.denominator
+
+  for x in res:
+    final.append( denominator/x.denominator * x.numerator )
+  final.append(denominator)
+
+  return final
